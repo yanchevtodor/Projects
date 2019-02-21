f7462e5c41a9144ce2948ab569b03d59d6d7267f
yanchevtodor <yanchev.todor@gmail.com>
2019-02-21 23:05:53 +0200

    some

--- JS exercises/Agrgates.js
+++
@@ -1,9 +0,0 @@
-function reduce (arr, func) {
-    let result = arr[0];
-    for (let nextNumber of arr.slice(1)){
-        result = func(result, nextNumber);
-
-    }
-    return result;
-}
-console.log(36*0.05);console.log(5*0.36);
---
+++ node/first/index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const port = 1234
+
+http
+    .createServer(function (req, res){
+      
+            
+    })
+    .listen(port);
+    console.log('node is listening')
