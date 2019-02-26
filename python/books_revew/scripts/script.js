a9bad2dd865159041c94821cea4404408de504ae
yanchevtodor <yanchev.todor@gmail.com>
2019-02-26 14:23:04 +0200

    Update script.js

--- python/books_revew/scripts/script.js
+++ python/books_revew/scripts/script.js
@@ -1,29 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelectorAll('.book_serch').forEach(div => {
- 
-            div.onclick = function () {            
-                console.log(this.dataset.search);            
-                $(".serch_input").load(`Templates/${this.dataset.search}.html`)  ;
-                $('#search').click(function(){
 
-              $(`#${this.dataset.search}`).onclick = function () { 
-              
-                  $(".results").load(`Templates/results.html`)  ;
-                   
+        div.onclick = function () {
+            console.log(this.dataset.search);
+            $(".serch_input").load(`Templates/${this.dataset.search}.html`);
+            $('#search').click(function () {
+
+                    $(`#${this.dataset.search}`).onclick = function () {
+
+                        $(".results").load(`Templates/results.html`);
 
 
-            };  
-        };
 
-        
+                    };
+                });
+
+
+            };
+
+        //    $('#search').click(function(){
+        //        
+        //          $(".results").load("Templates/browse.html")         
+        //
+        //        
+        //    });
+
     });
-    
-//    $('#search').click(function(){
-//        
-//          $(".results").load("Templates/browse.html")         
-//
-//        
-//    });
-
 });
