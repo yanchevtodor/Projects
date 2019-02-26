7220800847f708d048ee32f8716c27feddf0d1e4
yanchevtodor <yanchev.todor@gmail.com>
2019-02-26 12:04:36 +0200

    Add files via upload

---
+++ python/books_revew/scripts/script.js
@@ -0,0 +1,30 @@
+//document.addEventListener('DOMContentLoaded', () => {
+//
+//    document.querySelectorAll('.book_serch').forEach(div => {
+////        document.querySelector('.section_1 container').append()
+//        
+//        
+//        div.onclick = function () {
+//            
+//            console.log(this.dataset.search)
+////            document.querySelector('.section_1').load(`Templates/${this.dataset.search}.html`)
+////            
+//            console.log(`Templates/${this.dataset.search}.html`)
+//        };
+//
+//        
+//    });
+//
+//});
+
+//
+//document.addEventListener('DOMContentLoaded', () => {
+//
+//
+//    document.querySelectorAll('.color-change').forEach(button => {
+//        button.onclick = () => {
+//            document.querySelector('#hello').style.color = button.dataset.color;
+//        };
+//    });
+//
+//});
