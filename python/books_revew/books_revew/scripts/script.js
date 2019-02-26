49b91f855892b10d56817f480d20835ea5c3fb6d
yanchevtodor <yanchev.todor@gmail.com>
2019-02-26 12:22:24 +0200

    Add files via upload

---
+++ python/books_revew/books_revew/scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelectorAll('.book_serch').forEach(div => {
+ 
+        div.onclick = function () {            
+            console.log(this.dataset.search)            $(".serch_input").load(`Templates/${this.dataset.search}.html`)            
+        };
+
+        
+    });
+    
+//    document.querySelector('.book_serch').forEach(div => {
+// 
+//        div.onclick = function () {            
+//            console.log(this.dataset.search)            $(".serch_input").load(`Templates/${this.dataset.search}.html`)            
+//        };
+//
+//        
+//    });
+
+});
