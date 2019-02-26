49019d35eae1a0c0424f4948d7b2c055c7e85948
yanchevtodor <yanchev.todor@gmail.com>
2019-02-26 15:27:15 +0200

    Update script.js

--- python/books_revew/scripts/script.js
+++ python/books_revew/scripts/script.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(this.dataset.search);
             $(".serch_input").load(`Templates/${this.dataset.search}.html`);
             
-
-
-            };
             if($(".results").is(':empty')){
                 console.log("empty")
             }
             else{
                 $(".results").empty();
             }
+
+            };
+            
 
     });
 });
