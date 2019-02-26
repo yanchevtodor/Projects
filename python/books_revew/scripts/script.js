bb953cf6d05d2f25341f90a9b0847857c88f9dda
yanchevtodor <yanchev.todor@gmail.com>
2019-02-26 12:50:33 +0200

    Update script.js

--- python/books_revew/scripts/script.js
+++ python/books_revew/scripts/script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.book_serch').forEach(div => {
  
         div.onclick = function () {            
-            console.log(this.dataset.search);            $(".serch_input").load(`Templates/${this.dataset.search}.html`)          $('#search').onclick(function(){
+            console.log(this.dataset.search);            
+            $(".serch_input").load(`Templates/${this.dataset.search}.html`)  ;
+            $('#search').onclick(function(){
         
           $(".results").load("Templates/browse.html")         
 
