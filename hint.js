8ad13c65ee3bc3b09100339bfd2a19d862782cd1
yanchevtodor <yanchev.todor@gmail.com>
2019-03-14 09:49:33 +0200

    Update hint.js

--- hint.js
+++ hint.js
@@ -1,27 +1,27 @@
-var rows = 3;
-var cols = 3
-var counter = 1
+function remove_cell(rows, cols){
+    var counter = 1
+    let matrix = [];
+    for(var i=0; i < rows; i++) {
+        matrix.push([]);
+    }
+    for (var row = 0; row < rows; row++) {
+        for (var col = 0; col < cols; col++) {   
 
-let matrix = [];
-for(var i=0; i < rows; i++) {
-    matrix.push([]);
-}
-for (var row = 0; row < rows; row++) {
-    for (var col = 0; col < cols; col++) {   
+                if(counter == 5){
+                    matrix[row][col]= 0;
+                    counter++
+                }
 
-            if(counter == 5){
-                matrix[row][col]= 0;
-                counter++
-            }
+                else{
+                    matrix[row][col]= counter; 
+                    counter++
+                }
 
-            else{
-                matrix[row][col]= counter; 
-                counter++
-            }
+
+        }
 
 
     }
-    
-
+    console.log(matrix)
 }
-console.log(matrix)
+remove_cell(3,3)
